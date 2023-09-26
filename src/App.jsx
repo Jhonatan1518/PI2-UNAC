355b86df879d32a99f73f39d2af5495cc6bb3da9
Jhonatan Zapata <jhonatan.zapatah@unac.edu.co>
2023-09-26 15:49:19 -0500

    Create Docker File

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
-import LoginPage from './pages/LoginPage/LoginPAge';
+import LoginPage from './pages/LoginPage/LoginPage';
 import './App.css'
 import NavBar from './components/NavBar/NavBar';
 import ServiciosPage from './pages/ServiciesPage/ServiciosPage';
